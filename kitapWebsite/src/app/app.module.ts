56695bb88c3b62409e9d6aabedbc527d02e3c6e8
asalybek <luxuryteenager@gmail.com>
2020-04-09 21:58:54 +0600

    login page

--- kitapWebsite/src/app/app.module.ts
+++ kitapWebsite/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { CategoriesComponent } from './categories/categories.component';
 import { BooksCategoriesComponent } from './books-categories/books-categories.component';
 import { BookDetailComponent } from './book-detail/book-detail.component';
 import { ComicsDetailComponent } from './comics-detail/comics-detail.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,12 @@ import { ComicsDetailComponent } from './comics-detail/comics-detail.component';
     CategoriesComponent,
     BooksCategoriesComponent,
     BookDetailComponent,
-    ComicsDetailComponent
+    ComicsDetailComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
