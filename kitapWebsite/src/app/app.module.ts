92e237685a35a3fabf7c792cc9916b7f8d98b8be
Yevgeniya <ekim27220@gmail.com>
2020-04-09 20:28:36 +0600

    books-categories added

--- kitapWebsite/src/app/app.module.ts
+++ kitapWebsite/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { BooksCategoriesComponent } from './books-categories/books-categories.component';
 import { BookDetailComponent } from './book-detail/book-detail.component';
 import { ComicsDetailComponent } from './comics-detail/comics-detail.component';
+import { Route, RouterModule } from '@angular/router';
+
+const routes: Route[] = [
+  {path: 'books-categories', component: BooksCategoriesComponent},
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +22,9 @@ import { ComicsDetailComponent } from './comics-detail/comics-detail.component';
     ComicsDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
