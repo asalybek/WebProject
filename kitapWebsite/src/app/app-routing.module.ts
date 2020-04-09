115d816020240c56bcd204ac054727d8f3436a48
Yevgeniya <ekim27220@gmail.com>
2020-04-10 04:43:51 +0600

    changes added. many changes

--- kitapWebsite/src/app/app-routing.module.ts
+++ kitapWebsite/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksCategoriesComponent} from './books-categories/books-categories.component';
+import {BookDetailComponent} from './book-detail/book-detail.component';
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
-  { path: 'books-categories', component: BooksCategoriesComponent }
+  { path: 'books-categories', component: BooksCategoriesComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'books-categories/:categoryId/books', component: BookDetailComponent }
 ];
 
 @NgModule({
