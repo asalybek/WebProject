e6f4a23dd351ce701499aac61f9498c4d7bca9ba
asalybek <luxuryteenager@gmail.com>
2020-04-26 12:33:34 +0600

    top six comics

--- kitapWebsiteBack/comics/views_cbv.py
+++ kitapWebsiteBack/comics/views_cbv.py
@@ -1,10 +1,11 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
+from comics.filters import ComicsFilter
 from comics.models import ComicsCategory, Comics
 
 from comics.serializers import ComicsCategorySerializer2, ComicsSerializer2
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ComicsCategoryListAPIView(generics.ListAPIView):
     queryset = ComicsCategory.objects.all()
@@ -19,4 +20,7 @@ class ComicsDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 class ComicsListAPIView(generics.ListAPIView):
     queryset = Comics.objects.all()
     serializer_class = ComicsSerializer2
+    filter_backends = (DjangoFilterBackend,)
+    # filterset_fields = ('rating')
+    filter_class = ComicsFilter
 
